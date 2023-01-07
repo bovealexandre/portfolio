a2df22fad5b76fb98321682672b5804c66c498f6
Alexandre Bove <46528060+bovealexandre@users.noreply.github.com>
2023-01-07 09:28:47 +0100

    fix

--- src/entry.netlify-edge.tsx
+++ src/entry.netlify-edge.tsx
@@ -1,5 +1,14 @@
-import { createQwikCity } from '@builder.io/qwik-city/middleware/netlify-edge'
-import qwikCityPlan from '@qwik-city-plan'
-import render from './entry.ssr'
+/*
+ * WHAT IS THIS FILE?
+ *
+ * It's the  entry point for netlify-edge when building for production.
+ *
+ * Learn more about the cloudflare integration here:
+ * - https://qwik.builder.io/integrations/deployments/netlify-edge/
+ *
+ */
+import { createQwikCity } from '@builder.io/qwik-city/middleware/netlify-edge';
+import qwikCityPlan from '@qwik-city-plan';
+import render from './entry.ssr';
 
-export default createQwikCity({ render, qwikCityPlan })
+export default createQwikCity({ render, qwikCityPlan });
